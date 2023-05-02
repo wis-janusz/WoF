2793ecde505eecf6e65709acc9834b89ed8f712c
wis-janusz <wis.janusz@gmail.com>
2023-05-02 11:18:01 +0200

    added basic dash app

--- wof/__main__.py
+++ wof/__main__.py
@@ -1,6 +1,19 @@
+from dash import Dash, html, dash_table
 import utils
 
 local_projects_path = "local_projects.json"
 projects = utils.load_projects(local_projects_path)
 choice = projects.sample()
-print(choice)
+
+app = Dash("WoF")
+
+app.layout = html.Div([
+    html.Div(children='Local Projects'),
+    dash_table.DataTable(data=projects.to_dict('records'), page_size=10),
+    html.Div(children='Randomly Chosen Project'),
+    dash_table.DataTable(data=choice.to_dict('records'), page_size=10)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
+
